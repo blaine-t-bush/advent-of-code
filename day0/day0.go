1edc5ac9117acc397f1aa05e69afa1422b29a4d6
Blaine Bush <blaine.bush@gmail.com>
2022-12-05 11:08:32 +0100

    copy Alfred's start_solving script

---
+++ day0/day0.go
@@ -0,0 +1,36 @@
+package day0
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func getInput() {
+	f, err := os.Open("./day0/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println(text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SolvePartOne() {
+	getInput()
+}
+
+func SolvePartTwo() {
+	getInput()
+}
---
+++ start_solving.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+YEAR=$1
+DAY=$2
+
+mkdir -p 'day'$DAY
+
+URL='https://adventofcode.com/'$YEAR'/day/'$DAY
+INPUT_FILE=day$DAY/input.txt
+SOLUTION_FILE=day$DAY/day$DAY.go
+
+max_fails=10
+cur_fails=0
+until $(curl $URL'/input' --config aoc_session --output $INPUT_FILE --silent --fail --retry 10 --retry-delay 5)
+do
+    ((cur_fails++))
+    echo $cur_fails': Not quite yet...'
+    if [ $cur_fails -ge $max_fails ]
+    then
+       echo 'Puzzle not yet released, please be patient.'
+       exit 1
+    fi
+    sleep 5
+done
+
+echo "It's ready, start solving!"
+cp -i day0/day0.go $SOLUTION_FILE
+# Updates the day in the template
+sed -i -E "s/day0/day$DAY/" $SOLUTION_FILE
+cmd.exe /C start $URL
+# code -r $INPUT_FILE $SOLUTION_FILE
+code -r $SOLUTION_FILE
+# cd $YEAR
+# python day$DAY.py
