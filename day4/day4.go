3bb5c4e40185037fa8626cfe7ca64584a93868f9
Blaine Bush <blaine.bush@gmail.com>
2022-12-05 09:30:50 +0100

    day4 solutions

---
+++ day4/day4.go
@@ -0,0 +1,113 @@
+package day4
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Assignment struct {
+	ID    int
+	Start int
+	End   int
+}
+
+type Pair struct {
+	ID          int
+	Assignment1 Assignment
+	Assignment2 Assignment
+}
+
+func getInput() []Pair {
+	f, err := os.Open("./day4/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	pairCount := 0
+	pairs := []Pair{}
+	for scanner.Scan() {
+		text := scanner.Text()
+		pairs = append(pairs, parsePair(text, pairCount))
+		pairCount++
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return pairs
+}
+
+func parsePair(text string, pairCount int) Pair {
+	r, err := regexp.Compile(`^(?P<num1>\d+)-(?P<num2>\d+),(?P<num3>\d+)-(?P<num4>\d+)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := r.FindStringSubmatch(text)
+
+	start1, err := strconv.Atoi(m[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	end1, err := strconv.Atoi(m[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	start2, err := strconv.Atoi(m[3])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	end2, err := strconv.Atoi(m[4])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return Pair{
+		ID: pairCount,
+		Assignment1: Assignment{
+			ID:    pairCount * 2,
+			Start: start1,
+			End:   end1,
+		},
+		Assignment2: Assignment{
+			ID:    pairCount*2 + 1,
+			Start: start2,
+			End:   end2,
+		},
+	}
+}
+
+func SolvePartOne() int {
+	totalOverlapCount := 0
+	for _, pair := range getInput() {
+		if (pair.Assignment1.Start >= pair.Assignment2.Start && pair.Assignment1.End <= pair.Assignment2.End) || (pair.Assignment1.Start <= pair.Assignment2.Start && pair.Assignment1.End >= pair.Assignment2.End) {
+			totalOverlapCount++
+		}
+	}
+
+	fmt.Println(totalOverlapCount)
+	return totalOverlapCount
+}
+
+func SolvePartTwo() int {
+	partialOverlapCount := 0
+	for _, pair := range getInput() {
+		if !(pair.Assignment1.End < pair.Assignment2.Start || pair.Assignment2.End < pair.Assignment1.Start) {
+			partialOverlapCount++
+		}
+	}
+
+	fmt.Println(partialOverlapCount)
+	return partialOverlapCount
+}
--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	// day1 "github.com/blaine-t-bush/advent-of-code-2022/day1"
 	// day2 "github.com/blaine-t-bush/advent-of-code-2022/day2"
-	day3 "github.com/blaine-t-bush/advent-of-code-2022/day3"
+	// day3 "github.com/blaine-t-bush/advent-of-code-2022/day3"
+	day4 "github.com/blaine-t-bush/advent-of-code-2022/day4"
 )
 
 func main() {
-	day3.SolvePartOne()
-	day3.SolvePartTwo()
+	day4.SolvePartOne()
+	day4.SolvePartTwo()
 }
