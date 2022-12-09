638d73062ecd90b5002f78c60c0269afa1947ea5
Blaine Bush <blaine.bush@gmail.com>
2022-12-09 23:05:46 +0100

    add trail to 2022 day 9 viz

--- 2022/day9/viz.go
+++ 2022/day9/viz.go
@@ -17,9 +17,11 @@ const (
 )
 
 var (
-	knotImage = ebiten.NewImage(tileSize, tileSize)
-	w         = 640
-	h         = 640
+	knotImage  = ebiten.NewImage(tileSize, tileSize)
+	pathImage  = ebiten.NewImage(tileSize, tileSize)
+	w          = 640
+	h          = 640
+	pathsImage *ebiten.Image
 )
 
 type Game struct {
@@ -64,13 +66,14 @@ func (g *Game) init() {
 		}
 	}
 
-	w = tileSize * (util.MaxIntsSlice(xCoords) - util.MinIntsSlice(xCoords) + 20)
-	h = tileSize * (util.MaxIntsSlice(yCoords) - util.MinIntsSlice(yCoords) + 20)
+	w = tileSize * (util.MaxIntsSlice(xCoords) - util.MinIntsSlice(xCoords) + 50)
+	h = tileSize * (util.MaxIntsSlice(yCoords) - util.MinIntsSlice(yCoords) + 50)
 	ebiten.SetWindowSize(w, h)
 
 	g.resetState()
 
 	g.op = &ebiten.DrawImageOptions{}
+	pathsImage = ebiten.NewImage(w, h)
 }
 
 func (g *Game) Update() error {
@@ -106,6 +109,46 @@ func (g *Game) resetState() {
 			7: {0, 0},
 			8: {0, 0},
 			9: {0, 0},
+			// 10: {0, 0},
+			// 11: {0, 0},
+			// 12: {0, 0},
+			// 13: {0, 0},
+			// 14: {0, 0},
+			// 15: {0, 0},
+			// 16: {0, 0},
+			// 17: {0, 0},
+			// 18: {0, 0},
+			// 19: {0, 0},
+			// 20: {0, 0},
+			// 21: {0, 0},
+			// 22: {0, 0},
+			// 23: {0, 0},
+			// 24: {0, 0},
+			// 25: {0, 0},
+			// 26: {0, 0},
+			// 27: {0, 0},
+			// 28: {0, 0},
+			// 29: {0, 0},
+			// 30: {0, 0},
+			// 31: {0, 0},
+			// 32: {0, 0},
+			// 33: {0, 0},
+			// 34: {0, 0},
+			// 35: {0, 0},
+			// 36: {0, 0},
+			// 37: {0, 0},
+			// 38: {0, 0},
+			// 39: {0, 0},
+			// 40: {0, 0},
+			// 41: {0, 0},
+			// 42: {0, 0},
+			// 43: {0, 0},
+			// 44: {0, 0},
+			// 45: {0, 0},
+			// 46: {0, 0},
+			// 47: {0, 0},
+			// 48: {0, 0},
+			// 49: {0, 0},
 		},
 		tailVisitedCoords: []coord{
 			{0, 0},
@@ -133,6 +176,20 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		ebitenutil.DebugPrintAt(screen, "press space to start", w/2-10*fontWidth, h/2-fontWidth)
 	} else {
 		ebitenutil.DebugPrint(screen, fmt.Sprintf("current command: %d\ncurrent steps:   %d\nx: %d, y: %d", g.currentCommandCount, g.currentCommandProgress, g.rope.knots[0].x, g.rope.knots[0].y))
+
+		// draw traversed path
+		lastVisited := g.rope.tailVisitedCoords[len(g.rope.tailVisitedCoords)-1]
+		resized := coord{lastVisited.x * tileSize, lastVisited.y * tileSize}
+		translated := resized.translateToScreenCenter()
+		g.op.GeoM.Reset()
+		g.op.GeoM.Translate(float64(translated.x), float64(translated.y))
+		g.op.ColorM.Scale(1, 1, 1, 1)
+		pathImage.Fill(color.NRGBA{255, 0, 0, 50})
+		pathsImage.DrawImage(pathImage, g.op)
+		g.op.GeoM.Reset()
+		screen.DrawImage(pathsImage, g.op)
+
+		// draw knots
 		for i, knot := range g.rope.knots {
 			resized := coord{knot.x * tileSize, knot.y * tileSize}
 			translated := resized.translateToScreenCenter()
